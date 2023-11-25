e36f62d3c9e7b18764fed36157ed202f721b86b8
John Stuby <johnstuby7@gmail.com>
2023-11-24 17:33:35 -0700

    Initial commit for vue/firebase app, has basic login and signup pages

---
+++ src/views/Signup.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Sign Up</h1>
+    <input type="email" placeholder="Email" v-model="email">
+    <input type="password" placeholder="Password" v-model="password">
+    <button @click="signup">Sign Up</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignUp",
+  data() {
+    return {
+      email: "",
+      password: "",
+      initials: "",
+    }
+  },
+  methods: {
+    signup() {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
